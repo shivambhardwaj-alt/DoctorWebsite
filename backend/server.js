ba7b8c507d9d865d349f8dfff0e1d735a60d60f7
shivambhardwaj-alt <shubhambhardwaj20232024@gmail.com>
2026-07-17 14:14:59 +0530

    A more fix for the uploads folder

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDb from './config/connectDb.js'
 import { connect } from 'mongoose';
+import { fileURLToPath } from 'url'
+import fs from 'fs';
+import path from 'path';
 import connectCloudinary from './config/connectCloudinary.js';
 import adminRouter from './routes/adminRoutes.js';
 import userRouter from './routes/userRoutes.js';
@@ -11,10 +14,28 @@ import doctorRouter from './routes/doctorRoutes.js';
 import mailRouter from './routes/mailRouter.js';
 import smsRouter from './routes/smsRoutes.js';
 import './scheduler/scheduler.js'
-// create app
+
 const app = express()
 
 const port = process.env.PORT || 4000
+
+
+
+//  a temporary fix for the uploads (needed to fix)
+
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const uploadsDir = path.join(__dirname, 'uploads')
+
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true })
+  console.log('Created uploads folder at:', uploadsDir)
+} else {
+  console.log('Uploads folder already exists')
+}
 
 
 app.use(express.json())
